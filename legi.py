8ce2336d79f314c3a1eb3113eed906d4261d1bb1
drewablo <andrewt77@gmail.com>
2014-05-13 07:00:07 -0500

    Name error message and exit function adde

--- legi.py
+++ legi.py
@@ -1,10 +1,10 @@
 from bs4 import BeautifulSoup
 import urllib2
 import re
+from sys import exit
 
 def name():
-	nameID = {1911: ['Pamela J. Althoff'],2018:	['Jason A. Barickman'],2022:['Jennifer Bertino-Tarrant'],2020:['Daniel Biss'],1948:	['Tim Bivins'],1880:['William E. Brady'], 2023:['Melinda Bush'], 1864:['James F. Clayborne, Jr.'], 1899:['Jacqueline Y. Collins'], 2024: ['Michael Connelly'], 1865: ['John J. Cullerton'],2025:['Thomas Cullerton'],2026:['Bill Cunningham'],1930:	['William Delgado'],1866:['Kirk W. Dillard'],1960:['Dan Duffy'],1914:['Gary Forby'],1937:['Michael W. Frerichs'],1909:['William R. Haine'],1905:['Don Harmon'],2027:['Napoleon Harris, III'],2028:['Michael E. Hastings'],1936:	['Linda Holmes'],1910:['Mattie Hunter'],1961:['Toi W. Hutchinson'],1924:['Mike Jacobs'],1963:	['Emil Jones, III'],1938:['David Koehler'],1935:['Dan Kotowski'],1999:['Darin M. LaHood'],1997:	['Steven M. Landek'],1869:['Kimberly A. Lightford'],1870:['Terry Link'],1871:['David S. Luechtefeld'],2029: ['Andy Manar'],1902:['Iris Y. Martinez'],1995:['Wm. Sam McCann'],1964:	['Kyle McCarter'],2030:	['Karen McConnaughay'],2010:['Pat McGuire'],2031:['Julie A. Morrison'],1970:['John G. Mulroe'],1872:['Antonio Munoz'],1934:['Matt Murphy'],1933: ['Michael Noland'],2032: ['Jim Oberweis'],1873: ['Christine Radogno'],1917: ['Kwame Raoul'],1975: ['Sue Rezin'],1908:['Dale A. Righter'],2033:['Chapin Rose'],1897: ['Martin A. Sandoval'],1874:	['Ira I. Silverstein'],2034:['Steve Stadelman'],1947: ['Heather A. Steans'],1906: ['John M. Sullivan'],1875:['Dave Syverson'],1876:	['Donne E. Trotter'],2035:['Patricia Van Pelt']}
-
+	nameID = {1911: ['pamela j. althoff'],2018:['jason a. barickman'],2022:['jennifer bertino-tarrant'],2020:['daniel biss'],1948:	['tim bivins'],1880:['william e. brady'], 2023:['melinda bush'], 1864:['james f. clayborne, jr.'], 1899:['jacqueline y. collins'], 2024: ['michael connelly'], 1865: ['john j. cullerton'],2025:['thomas cullerton'],2026:['bill cunningham'],1930:	['william delgado'],1866:['kirk w. dillard'],1960:['dan duffy'],1914:['gary forby'],1937:['michael w. frerichs'],1909:['william r. haine'],1905:['don harmon'],2027:['napoleon harris, iii'],2028:['michael e. hastings'],1936:	['linda holmes'],1910:['mattie hunter'],1961:['toi w. hutchinson'],1924:['mike jacobs'],1963:	['emil jones, iii'],1938:['david koehler'],1935:['dan kotowski'],1999:['darin m. lahood'],1997:	['steven m. landek'],1869:['kimberly a. lightford'],1870:['terry link'],1871:['david s. luechtefeld'],2029: ['andy manar'],1902:['iris y. martinez'],1995:['wm. sam mccann'],1964:	['kyle mccarter'],2030:	['karen mcconnaughay'],2010:['pat mcguire'],2031:['julie a. morrison'],1970:['john g. mulroe'],1872:['antonio munoz'],1934:['matt murphy'],1933: ['michael noland'],2032: ['jim oberweis'],1873: ['christine radogno'],1917: ['kwame raoul'],1975: ['sue rezin'],1908:['dale a. righter'],2033:['chapin rose'],1897: ['martin a. sandoval'],1874:	['ira i. silverstein'],2034:['steve stadelman'],1947: ['heather a. steans'],1906: ['john m. sullivan'],1875:['dave syverson'],1876:	['donne e. trotter'],2035:['patricia van pelt']}
 	return nameID
 	
 def main():
@@ -23,34 +23,42 @@ def main():
 			new_id = [val for subl in id for val in subl]
 			new_id = map(int, new_id)
 	dirt = zip(new_id,links)
-	direct = dict(dirt)
-	return direct
-	
+	directory = dict(dirt)
+	return directory
 	
 def ask_name (nameID):
-	member = str(raw_input("Who are you looking for "))
-	for nameIDkey, value in nameID.items():
-		for v in value:
-			if member in v:
-				return nameIDkey
-	
-def individual(direct,nameIDkey):
-	url2 = urllib2.urlopen(direct[nameIDkey])
-	content2 = url2.read()
-	soup2 = BeautifulSoup(content2)
-	table2 = soup2.find('table', cellpadding=3)
-	for item in table2.findAll('tr')[1:]:
-			col = item.findAll('td')
-			sponsor = col[1].string
-			last_action = col[4].string
-			sponsor = sponsor.encode('ascii','ignore')
-			#link = re.findall(r'(http?://\S+)',links)
-			if re.findall('Public Act', last_action):
-				if (col[0].string is None) or (sponsor is None):
-					print 'error'
-					print col[0].string +' '+sponsor
-				else:
-					print col[0].string +' '+sponsor
+	member = str(raw_input("Who are you looking for ").lower())
+	if member == 'exit':
+		exit(0)
+	else:
+		for nameIDkey, value in nameID.items():
+			for v in value:
+				if member in v:
+					return nameIDkey
+		
+def individual(directory,nameIDkey):
+	legisl = directory.get(nameIDkey)
+	if legisl is None:
+		print "Legislator does not exist"
+	else:
+		url2 = urllib2.urlopen(directory[nameIDkey])
+		content2 = url2.read()
+		soup2 = BeautifulSoup(content2)
+		table2 = soup2.find('table', cellpadding=3)
+		for item in table2.findAll('tr')[1:]:
+				col = item.findAll('td')
+				sponsor = col[1].string
+				last_action = col[4].string
+				sponsor = sponsor.encode('ascii','ignore')
+				#link = re.findall(r'(http?://\S+)',links)
+				bill = col[0].string
+				if re.findall('Public Act', last_action):
+					if (col[0].string is None) or (sponsor is None):
+						print 'error'
+					else:
+						print bill+' '+sponsor
+
 
 while True:
+	print "Search by either first or last name, but not both"
 	individual(main(),ask_name(name()))	

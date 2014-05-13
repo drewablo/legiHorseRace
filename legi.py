898d5bc9f96c8ef834a8f840815723a568ad3270
drewablo <andrewt77@gmail.com>
2014-05-13 10:47:42 -0500

    added categories and bill count
    I added the ability to select between a public act, passed both chambers and stuck in assignments/rules. Additionally, it now spits out a bill count for whatever you search for.

--- legi.py
+++ legi.py
@@ -14,16 +14,18 @@ def main():
 	url = urllib2.urlopen("http://www.ilga.gov/senate")
 	content = url.read()
 	soup = BeautifulSoup(content)
+	append1 = links.append
+	append2 = id.append
 	for a in soup.findAll('a',href=True):
-		if re.findall('SenatorBills', a['href']):
+		if re.findall('Bills', a['href']):
 			l = ("http://www.ilga.gov/senate/" + a['href']+'&Primary=True')
-			links.append(str(l))
-			id.append(re.findall('\d+', links[x]))
+			append1(str(l))
+			append2(re.findall('\d+', links[x]))
 			x+=1
 			new_id = [val for subl in id for val in subl]
 			new_id = map(int, new_id)
-	dirt = zip(new_id,links)
-	directory = dict(dirt)
+	dirct = zip(new_id,links)
+	directory = dict(dirct)
 	return directory
 	
 def ask_name (nameID):
@@ -34,31 +36,51 @@ def ask_name (nameID):
 		for nameIDkey, value in nameID.items():
 			for v in value:
 				if member in v:
-					return nameIDkey
-		
+					return nameIDkey	
 def individual(directory,nameIDkey):
 	legisl = directory.get(nameIDkey)
+	bill_count = 0
 	if legisl is None:
-		print "Legislator does not exist"
+		print "LEGISLATOR NOT FOUND"
 	else:
-		url2 = urllib2.urlopen(directory[nameIDkey])
-		content2 = url2.read()
-		soup2 = BeautifulSoup(content2)
-		table2 = soup2.find('table', cellpadding=3)
-		for item in table2.findAll('tr')[1:]:
+		url = urllib2.urlopen(directory[nameIDkey])
+		content = url.read()
+		soup = BeautifulSoup(content)
+		table = soup.find('table', cellpadding=3)
+		la = str(raw_input('Public Act = p, Passed both chambers = bc, or Stuck in the system = r :'))
+		for item in table.findAll('tr')[1:]:
 				col = item.findAll('td')
 				sponsor = col[1].string
 				last_action = col[4].string
 				sponsor = sponsor.encode('ascii','ignore')
 				#link = re.findall(r'(http?://\S+)',links)
 				bill = col[0].string
-				if re.findall('Public Act', last_action):
-					if (col[0].string is None) or (sponsor is None):
-						print 'error'
-					else:
-						print bill+' '+sponsor
-
-
+				if la == 'p':
+					for x in re.findall('Public Act', last_action):
+						if (col[0].string is None) or (sponsor is None):
+							print 'error'
+						else:
+							bill_count+=1
+							print bill+' '+sponsor	
+				elif la == 'bc':
+					for x in re.findall('Both', last_action):
+						if (col[0].string is None) or (sponsor is None):
+							print 'error'
+						else:
+							bill_count+=1
+							print bill+' '+sponsor
+				elif la == 'r':
+					if re.findall('ferred', last_action):
+						if (col[0].string is None) or (sponsor is None):
+							print 'error'
+						else:
+							bill_count+=1
+							print bill+' '+sponsor
+				else:
+					print 'Please enter either p, bc, or r'
+					la = str(raw_input('Public Act = p, Passed both chambers = bc, or Stuck in the system = r :'))
+	print 'BILLS FOUND: ',bill_count
+	
+print "Search by either first or last name, but not both"
 while True:
-	print "Search by either first or last name, but not both"
 	individual(main(),ask_name(name()))	
